119138d7be73aa3333be66a609b33a22de79490c
LuShanshan <162866667+shan2lu@users.noreply.github.com>
2025-03-12 22:24:59 +0800

    Delete ICustomer.java

--- ICustomer.java
+++
@@ -1,7 +0,0 @@
-/**
-* The ICustomer interface defines the contract for a customer-related functionality.
-* It contains a single method, start(), which is expected to initiate the customer operations.
-*/
-public interface ICustomer {
-	public void start();
-}
