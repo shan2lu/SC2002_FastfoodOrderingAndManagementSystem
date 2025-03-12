d6dd1931905a0b3b114fac933509ddc18e14fdda
LuShanshan <162866667+shan2lu@users.noreply.github.com>
2025-03-12 22:28:15 +0800

    Delete OrderStatus.java

--- OrderStatus.java
+++
@@ -1,9 +0,0 @@
-/**
- * An enumeration representing the different stages of an order's status.
- */
-public enum OrderStatus {
-    ORDER_RECEIVED,
-    PREPARING,
-    READY_FOR_COLLECTION,
-    COLLECTED;
-}
