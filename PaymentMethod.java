4a3f708e136a41718bcbef1db9be2461652aa3ee
LuShanshan <162866667+shan2lu@users.noreply.github.com>
2025-03-12 22:28:32 +0800

    Delete PaymentMethod.java

--- PaymentMethod.java
+++
@@ -1,7 +0,0 @@
-/**
- * An abstract class for different payment methods to extend
- */
-public abstract class PaymentMethod {
-	public abstract boolean processPayment(double amount);
-	public abstract String getName();
-}
